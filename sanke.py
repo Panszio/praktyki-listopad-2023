470ae508b5c571d0d0ad0be7ecab201ca439a1b0
Panszio <samuel.jan.lis@gmail.com>
2023-11-09 15:54:14 +0100

    path snake

--- sanke.py
+++ sanke.py
@@ -3,7 +3,7 @@ from pygame.math import Vector2
 
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(5, 10),Vector2(6, 10),Vector2(7, 10)]
+        self.body = [Vector2(5, 10),Vector2(4, 10),Vector2(3, 10)]
         self.direction = Vector2(1, 0)
         self.new_block = False
 
@@ -51,6 +51,7 @@ class MAIN:
     def update(self):
         self.snake.move_snake()
         self.check_collision()
+        self.check_fail()
 
     def draw_elements(self):
             self.snake.draw_snake()
@@ -61,9 +62,16 @@ class MAIN:
             self.fruit.randomize()
             self.snake.add_block()
 
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            for block in self.snake.body[1:]:
+                if block == self.snake.body[0]:
+                    self.game_over()
 
 
-
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
 
 
 
